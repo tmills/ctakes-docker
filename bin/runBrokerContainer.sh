5b7b90d0fd43f26c9ca765357bbcc2a1d95ccf0d
James Masanz <jamesmasanz@users.noreply.github.com>
2017-10-23 21:00:31 -0400

    Update runBrokerContainer.sh
    issue error if env_file.txt does not exist

--- bin/runBrokerContainer.sh
+++ bin/runBrokerContainer.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-. env_file.txt
+envfile="env_file.txt"
+if [ -r "$envfile" ]; then
+  . env_file.txt
+else
+  echo "File $envfile not found!"
+  exit 1
+fi
+# The following maps $broker_port to 61616 which should match what 
+# is EXPOSEd by the Dockerfile for the broker
 docker run -d -p $broker_port:61616 amq-image
