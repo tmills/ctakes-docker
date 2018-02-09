fb639f44fb19714de7afb190817d7eedc22f8a04
Tim Miller <timothy.miller@childrens.harvard.edu>
2018-02-09 16:12:13 -0500

    Unexpected and unused import of a scala math package deleted.

--- src/main/java/PrecisionLinkAnaforaReader.java
+++ src/main/java/PrecisionLinkAnaforaReader.java
@@ -16,7 +16,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import scala.math.Ordering;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
