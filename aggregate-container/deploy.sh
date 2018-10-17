b6de0cd0fba18211e505ecbe017cfb6ecb0576a8
Tim Miller <timothy.miller@childrens.harvard.edu>
2018-10-17 10:41:18 -0400

    Needed additions to aggregate container.

--- aggregate-container/deploy.sh
+++ aggregate-container/deploy.sh
@@ -8,4 +8,6 @@ fi
 
 keytool -importcert -noprompt -file shared/broker_cert -alias brokerCA -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -storepass changeit
 
+java -Dbroker_host="$broker_host" -Dbroker_port="$broker_port" -cp "code.jar:/apache-uima-as-2.10.3/lib/*:." GenerateDeploymentDescriptors.java
+
 ./deployAsyncService.sh desc/aggregateDeploymentDescriptor.xml -brokerURL "failover:https://$broker_host:$broker_port"
