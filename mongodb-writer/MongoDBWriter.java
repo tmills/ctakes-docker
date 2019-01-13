f1aaca0fbe314d98f34f4b13e09d1ab05576300c
Tim Miller <tim.miller@gmail.com>
2019-01-13 13:01:47 -0500

    WIP: Started skeleton of mongodb writer

---
+++ mongodb-writer/MongoDBWriter.java
@@ -0,0 +1,196 @@
+import org.apache.ctakes.core.pipeline.PipeBitInfo;
+import org.apache.ctakes.core.util.OntologyConceptUtil;
+import org.apache.ctakes.typesystem.type.refsem.Event;
+import org.apache.ctakes.typesystem.type.textsem.EventMention;
+import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
+import org.apache.ctakes.typesystem.type.textspan.Sentence;
+import org.apache.uima.UimaContext;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Logger;
+import org.apache.uima.util.Level;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Indexes;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ConnectionString;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+public class MongoDBWriter extends JCasAnnotator_ImplBase {
+
+    public static final String PARAM_MONGO_HOST = "MongoHost";
+    @ConfigurationParameter(name = PARAM_MONGO_HOST, mandatory=false)
+    private String mongoHost="localhost";
+
+    public static final String PARAM_MONGO_DB = "MongoDb";
+    @ConfigurationParameter(name = PARAM_MONGO_DB, mandatory=false)
+    private String mongoDb = "mydb";
+
+    public static final String PARAM_MONGO_COLLECTION = "MongoCollection";
+    @ConfigurationParameter(name = PARAM_MONGO_COLLECTION, mandatory=false)
+    private String collection = "cuis";
+
+    public static final String CUI_FIELD = "cui";
+    public static final String ENCOUNTER_FIELD = "encounter";
+    public static final String PT_FIELD = "patient_num";
+    public static final String PROVIDER_FIELD = "provider";
+    public static final String SOURCESYSTEM_FIELD = "sourcesystem_cd";
+
+    static private final String CTAKES_VERSION = "CTAKES_4.0.0_PL_171129";
+
+
+    private MongoClient mongoClient = null;
+    private MongoDatabase mongoDb = null;
+    private MongoCollection<Document> coll = null;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(UimaContext context) throws ResourceInitializationException {
+        super.initialize(context);
+    
+        // Initialize the MongoDB Connection
+        try{
+            this.mongoClient = MongoClients.create("mongodb://" + this.mongoHost);
+            this.mongoDb = mongoClient.getDatabase(this.mongoDb);
+            this.coll = mongoDb.getCollection(this.collection);
+        }catch(Exception e){
+            throw new ResourceInitializationException(e);
+        }
+    }
+
+    /**
+        * {@inheritDoc}
+        */
+    @Override
+    public void process( final JCas jcas ) throws AnalysisEngineProcessException {
+        final SourceData sourceData = SourceMetadataUtil.getSourceData( jcas );
+        if ( sourceData == null ) {
+            LOGGER.log(Level.SEVERE,  "Missing source metadata for document!" );
+            return;
+        }
+        final long patientNum = SourceMetadataUtil.getPatientNum( jcas );
+        final int encounterNum;
+        try {
+            encounterNum = SourceMetadataUtil.getEncounterNum( sourceData );
+        } catch (ResourceProcessException e) {
+            throw new AnalysisEngineProcessException(e);
+        }
+        final String providerId = SourceMetadataUtil.getProviderId( sourceData );
+        final Timestamp startDate = SourceMetadataUtil.getStartDate( sourceData );
+
+        JCas deidView = null;
+        try {
+            deidView = jcas.getView("DeidView");
+        } catch(CASException e) {
+            throw new RuntimeException(e);
+        }
+
+		final Collection<IdentifiedAnnotation> annotations = JCasUtil.select(jcas, IdentifiedAnnotation.class);
+		if (annotations.isEmpty()) {
+			logger.log(Level.WARNING, "No annotations found. Patient, encounter, provider =  " + patient + ", " + encounter + ", " + provider);
+			return;
+		}
+
+        String fullEncounter = encounterNum+"_"+patientNum;
+
+        List<Document> docs = new ArrayList<>();
+        // iterate over jcas identified annotations creating new documents
+        for(IdentifiedAnnotation ent : annotations){
+			final Set<String> codes = new HashSet<String>(); 
+            // A Cui may belong to multiple Tuis, making multiple UmlsConcept objects (one per tui).
+            // I2b2 does NOT want multiple rows of a single Cui just because it has multiple tuis.
+            // FIXME: Can make the TUI field be a list?
+			codes.addAll(OntologyConceptUtil.getCuis(annotation));
+            for(String code : codes){
+                Document doc = new Document()
+                                    .append(CUI_FIELD, code)
+                                    .append(PT_FIELD, patientNum)
+                                    .append(ENCOUNTER_FIELD, encounter)
+                                    .append(SOURCESYSTEM_FIELD, CTAKES_VERSION)
+                                    .append(START_FIELD, ent.getBegin())
+                                    .append(END_FIELD, ent.getEnd())
+                                    .append(POLARITY_FIELD, ent.getPolarity())
+                                    .append(UNCERTAINTY_FIELD, ent.getUncertainty())
+                                    .append(DOCTIMEREL_FIELD, getDocTimeRel(ent))
+                                    .append(SENT_FIELD, getCoveringSent(ent));
+                docs.add(doc);
+            }
+        }
+        this.coll.insertMany(docs);
+    }
+
+    @Override
+    public void collectionProcessComplete() throws AnalysisEngineProcessException {
+        // Create an index over the 'cui' field for faster searching
+        this.coll.createIndex(Indexes.text(CUI_FIELD));
+    }
+
+	static private String getDocTimeRel(final IdentifiedAnnotation annotation) {
+		if (annotation == null || !(annotation instanceof EventMention)) {
+			return "";
+		}
+		final Event event = ((EventMention) annotation).getEvent();
+		if (event != null && event.getProperties() != null && event.getProperties().getDocTimeRel() != null) {
+			return event.getProperties().getDocTimeRel();
+		}
+		return "";
+	}
+
+    // FIXME, selectCovering is known to be slow, this could potentially slow down writes
+    static private String getSentenceCoverage( final JCas jcas, final IdentifiedAnnotation annotation ) {
+        final Collection<Sentence> entitySentences = JCasUtil.selectCovering( jcas,
+        	            Sentence.class, annotation.getBegin(), annotation.getEnd() );
+        int MAX_TVAL_LEN = 4000; 
+        int window = MAX_TVAL_LEN / 2; // count of characters not bytes
+        if (annotation.getCoveredText().length() > window) {
+            logger.log(Level.WARNING,"Annotation is longer than MAX_TVAL_LEN / 2. MAX_TVAL_LEN = " + MAX_TVAL_LEN + "\n'" + annotation.getCoveredText() + "'");
+            return annotation.getCoveredText().substring(0, window);
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        for ( Sentence sentence : entitySentences ) {
+            sb.append( sentence.getCoveredText() ).append( "\n" );
+        }
+        String s = sb.toString().trim();
+        if (s.length() <=  window) { 
+            return s;
+        }
+        logger.log(Level.WARNING,"SentenceCoverage is longer than MAX_TVAL_LEN / 2. MAX_TVAL_LEN = " + MAX_TVAL_LEN + "\n'" + s + "'");
+
+        // get a window around the entity that is at most 'window' long
+        // For this unusual case, only look at the first sentence covering the annotation
+        for ( Sentence sentence : entitySentences ) {
+            int begin = sentence.getBegin();
+            int end = sentence.getEnd();
+            if ((end - begin) > window) {
+                begin = (annotation.getEnd() - (window/2));
+                if (begin < sentence.getBegin()) begin = sentence.getBegin();
+                if (begin > annotation.getBegin()) return sentence.getCoveredText().substring(0, window);
+                end = annotation.getEnd() + (window/2);
+                if (end > sentence.getEnd()) end = sentence.getEnd();
+
+            }
+            return sentence.getCoveredText().substring(begin - sentence.getBegin(), end - sentence.getBegin());
+        }
+          
+        // if no sentence found covering the annotation, return just the annotation's covered text.
+        // This should never happen so don't bother with doing anything fancy like returning a larger string that 
+        // includes annotation in the middle.
+        logger.log(Level.SEVERE, "No sentence found covering annotation\n" + annotation + "\n");
+        return annotation.getCoveredText(); // verified earlier in this method that this is not longer than window
+	}
+
+}
